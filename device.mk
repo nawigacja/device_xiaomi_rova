171bd3c3e89acedfa4ee21e58173f8d5e148709e
xyzuan <xyzuannihboss@gmail.com>
2021-09-07 16:16:36 +0700

    rova: overlay-xd: declare `rova` specs
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -8,7 +8,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS +=  \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-xd
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 

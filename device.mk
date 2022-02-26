82e415a013cb4055ad882eada400e9412eb8957a
Kingsman44 <shivan.0972@gmail.com>
2022-02-26 22:37:50 +0530

    rova: audio_effects: add haptics generator
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
     libaudioroute \
     libaacwrapper \
     libaudio-resampler \
+    libhapticgenerator \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \

bb549c4ddcca8038328a2f1f7ebb08c6c7aa40f5
ItoRenz <itorenz@nusantararom.org>
2022-06-17 16:00:28 +0000

    rova: Voltage bringup
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_rova.mk
+    $(LOCAL_DIR)/voltage_rova.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_rova-user \
-    nad_rova-userdebug \
-    nad_rova-eng
+    voltage_rova-user \
+    voltage_rova-userdebug \
+    voltage_rova-eng
